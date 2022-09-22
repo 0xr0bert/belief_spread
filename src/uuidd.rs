8ea100e4b3b3329b74ecaff74982d7c2da79a82c
Robert Greener <me@r0bert.dev>
2022-09-22 14:04:54 +0100

    UUIDd: Change documentation to ref object
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/uuidd.rs
+++ src/uuidd.rs
@@ -4,10 +4,10 @@ use uuid::Uuid;
 ///
 /// This trait can be used by things that should have a [Uuid].
 pub trait UUIDd {
-    /// Get the UUID of the struct that implements this.
+    /// Get the UUID of the object that implements this.
     fn uuid(&self) -> Uuid;
 
-    /// Set the UUID of the struct that implements this.
+    /// Set the UUID of the object that implements this.
     ///
     /// # Arguments
     ///

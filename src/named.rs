3ee1985427cf9c5466949b68db1ff2af15ffb98f
Robert Greener <me@r0bert.dev>
2022-09-22 14:06:46 +0100

    named: Add set_name
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/named.rs
+++ src/named.rs
@@ -4,4 +4,10 @@
 pub trait Named {
     /// Get the name of the object with this trait.
     fn name(&self) -> &str;
+
+    /// Set the name of the object with this trait.
+    ///
+    /// # Arguments
+    /// - `name`: The new name as a [String].
+    fn set_name(&mut self, name: String);
 }

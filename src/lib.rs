1cdf2fdba3d08c3d54ede8adf5edc7555c9a42ca
Robert Greener <me@r0bert.dev>
2022-09-27 19:47:58 +0100

    Temporarily remove agent
    Signed-off-by: Robert Greener <me@r0bert.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 //! A library for modelling how beliefs spread through social networks.
-mod agent;
 mod behaviour;
 mod belief;
 pub mod errors;
 mod named;
 mod uuidd;
 
-pub use crate::agent::{Agent, BasicAgent};
 pub use crate::behaviour::{BasicBehaviour, Behaviour};
 pub use crate::belief::{BasicBelief, Belief};
 pub use crate::named::Named;
